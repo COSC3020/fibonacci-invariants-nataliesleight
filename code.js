c4671023271c27a8076003b546a3d58fceadc596
nataliesleight <nsleight@uwyo.edu>
2025-02-09 13:20:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 function fib(n) {
-    return n;
+    if (n == 0) {var arr = [0]; return arr;}
+    else {
+    var arr = [0,1];
+    var length = n;
+    fibonacci(n,arr, length);
+    return arr;}
+}
+
+function fibonacci(n, arr, length) {
+    if (n == 1) {return;}
+    arr.push(arr[length-n] + arr[length-n+1]);
+    n--;
+    return fibonacci(n, arr, length);
 }
